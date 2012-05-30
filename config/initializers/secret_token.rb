e800237fa2b3fdead71b50fa2b0bf9476c3eb660
Alex Aitken <alex.aitken@factorylabs.com>
2012-05-30 17:27:22 -0600

    initial commit, with network-connection speed work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+POCs::Application.config.secret_token = 'c560a09728c05675905e46623051403356f84b29e3d0f656e85afc0769b7f20bc13a53c0af9753470336ce422d5d2d89b72fb72d10b6ca2c5e3531846736c064'
